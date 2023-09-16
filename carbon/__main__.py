d4b7d9802cd69115f627c46c4ef6725b1af49538
Axe Botz <136898915+Axe-Botz@users.noreply.github.com>
2023-09-16 21:45:39 +0530

    Update __main__.py

--- carbon/__main__.py
+++ carbon/__main__.py
@@ -20,7 +20,24 @@ async def make_carbon(code):
         image = BytesIO(await resp.read())
     image.name = "carbon.png"
     return image
-  
+
+def split_limits(text):
+    if len(text) < 2048:
+        return [text]
+
+    lines = text.splitlines(True)
+    small_msg = ""
+    result = []
+    for line in lines:
+        if len(small_msg) + len(line) < 2048:
+            small_msg += line
+        else:
+            result.append(small_msg)
+            small_msg = line
+
+    result.append(small_msg)
+
+    return result
 
 def capture_err(func):
     @wraps(func)
